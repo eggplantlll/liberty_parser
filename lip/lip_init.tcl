90ef610a2f768653646e3201a01d01d20ae66cb7
yorkwuo <yorkwuo@gmail.com>
2018-03-22 20:54:40 +0800

    new

---
+++ lip/lip_init.tcl
@@ -0,0 +1,7 @@
+package provide lip 1.0
+
+# lip located in dir
+set dir [file dirname [info script]]
+
+source $dir/lip.tcl
+load $dir/lip.so
---
+++ lip/pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded lip 1.0 [list source [file join $dir lip_init.tcl]]
